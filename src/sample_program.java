191940e8b7626623bdf8ee54e743db3ba5ac6d8f
Your Name <your-email@example.com>
2025-08-29 11:23:54 +0530

    commit01

--- src/sample_program.java
+++ src/sample_program.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class sample_program {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hi");
+    }
 }
