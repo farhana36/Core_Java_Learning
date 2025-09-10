e3d8e2437eceb339f8d933a1deff398af30b5f5d
Your Name <your-email@example.com>
2025-09-10 16:49:39 +0530

    commit07

---
+++ src/Loop/FizzBizz.java
@@ -0,0 +1,28 @@
+package Loop;
+/*
+Program: Fizz-Buzz range from 1-50
+when the number is divisible by 2 --> print "Fizz"
+when the number is divisible by 5 --> print "Buzz"
+when the number is divisible by 2 & 5 --> "FizzBuzz"
+for remaining number print the "number"
+ */
+public class FizzBizz {
+    public static void main(String[] args) {
+        int num =50;
+        for(int i =1; i<=num; i++)
+        {
+            if(i%2==0){
+                System.out.println(i +" FIZZ");
+            }
+            else if(i%5==0) {
+                System.out.println(i +" Buzz");
+            }
+            else if (i%2==0 && i%5==0){
+                System.out.println(i +" FizzBuzz");
+                }
+            else{
+                System.out.println(i);
+        }
+        }
+    }
+}
